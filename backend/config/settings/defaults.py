9c48c07645ef55c13e5249f1a66737164af145af
Ludovic Ortega <mastership@hotmail.fr>
2019-05-30 20:38:23 +0300

    Fix issue regarding scheme CORS_ORIGIN_WHITELIST

--- backend/config/settings/defaults.py
+++ backend/config/settings/defaults.py
@@ -157,9 +157,9 @@ REST_FRAMEWORK = {
 
 # Server Headers settings
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8080'
-)
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:8080',
+]
 CORS_ALLOW_METHODS = (
     'GET',
     'POST',
